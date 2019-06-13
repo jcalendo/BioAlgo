7c5bb1891edd4903efd05188708335ae49956cc8
jcalendo <jcalendo@gmail.com>
2019-06-13 15:15:48 -0400

    added number_to_pattern algorithm

--- BioAlgo/pattern_counting.py
+++ BioAlgo/pattern_counting.py
@@ -91,3 +91,27 @@ def pattern_to_number(pattern: str) -> int:
     prefix = pattern.upper()[:-1]
 
     return 4 * pattern_to_number(prefix) +  symbol_dict.get(symbol)
+
+
+def number_to_pattern(index: int, k: int) -> str:
+    """Convert an integer to its pattern representation
+    
+    Arguments:
+        index {int} -- integer to convert to pattern
+        k {int} -- size of pattern
+
+    Returns:
+        str -- pattern
+    """
+    symbols = ["A", "C", "G", "T"]
+
+    if k == 1:
+        return symbols[index]
+
+    prefix_index = index // 4
+    r = index % 4
+    symbol = symbols[r]
+
+    prefix_pattern = number_to_pattern(prefix_index, k-1)
+
+    return prefix_pattern + symbol
--- tests/test_pattern_counting.py
+++ tests/test_pattern_counting.py
@@ -7,14 +7,14 @@ from BioAlgo import pattern_counting as pc
 def test_pattern_count():
     assert pc.pattern_count("ACAACTATGCATACTATCGGGAACTATCCT", "ACTAT") == 3
 
-
 def test_frequent_words():
     assert pc.frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == ['ACTAT']
-
 
 def test_fast_frequent_words():
     assert pc.frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == ['ACTAT']
 
-
 def test_pattern_to_number():
     assert pc.pattern_to_number("AGT") == 11
+
+def test_number_to_pattern():
+    assert pc.number_to_pattern(11, 3) == "AGT"
