c7e2f481664eafbe8d52132ce1033bba909dd3e8
jcalendo <jcalendo@gmail.com>
2019-06-14 18:47:24 -0400

    refactored

---
+++ BioAlgo/stringAlgorithms/frequentWords.py
@@ -0,0 +1,95 @@
+from typing import Set
+from itertools import product
+
+from patternCount import pattern_count
+from patternToNumber import pattern_to_number
+from numberToPattern import number_to_pattern
+
+
+def sorted_frequent_words(text: str, k: int) -> Set[str]:
+    """Find the most frequent kmers of size k in the given text by
+    sorting
+    
+    Arguments:
+        text {str} -- text to search
+        k {int} -- k-mer size
+    
+    Returns:
+        Set[str] -- most frequent k-mers in text
+    """
+    frequent_patterns = set()
+    index = []
+    count = []
+    for i in range(len(text) - k):
+        pattern = text[i:i+k]
+        index.append(pattern_to_number(pattern))
+        count.append(1)
+
+    sorted_index = sorted(index)
+
+    for i in range(1, len(text) - k):
+        if sorted_index[i] == sorted_index[i-1]:
+            count[i] = count[i-1] + 1
+
+    max_count = max(count)
+
+    for i in range(len(text) - k):
+        if count[i] == max_count:
+            pattern = number_to_pattern(sorted_index[i], k)
+            frequent_patterns.add(pattern)
+
+    return frequent_patterns
+
+
+def fast_frequent_words(text: str, k: int) -> Set[str]:
+    """Find the most frequent kmers of size k in the given text looping
+    through text only once.
+    
+    Arguments:
+        text {str} -- text to search
+        k {int} -- k-mer size
+    
+    Returns:
+        Set[str] -- most frequent k-mers in text
+    """
+    frequent_patterns = set()
+    kmers = list(product("ACGT", repeat=k))
+    freq_dict = {"".join(m):0 for m in kmers}
+
+    for i in range(len(text) - k):
+        pattern = text[i:i+k]
+        freq_dict[pattern] += 1
+
+    max_count = max(freq_dict.values())
+
+    for k in freq_dict:
+        if freq_dict[k] == max_count:
+            frequent_patterns.add(k)
+
+    return frequent_patterns
+
+
+def frequent_words(text: str, k: int) -> Set[str]:
+    """Find the most frequent kmers of size k in the given text
+    
+    Arguments:
+        text {str} -- text to find k-mer in
+        k {int} -- size of k-mer
+    
+    Returns:
+        Set[str] -- Set of most frequent k-mers found in text
+    """
+    frequent_patterns = set()
+    counts = []
+
+    for i in range(len(text) - k):
+        pattern = text[i:i+k]
+        counts.append(pattern_count(text, pattern))
+
+    max_count = max(counts)
+
+    for i in range(len(text) - k):
+        if counts[i] == max_count:
+            frequent_patterns.add(text[i:i+k])
+    
+    return frequent_patterns
--- BioAlgo/stringAlgorithms/numberToPattern.py
+++ BioAlgo/stringAlgorithms/numberToPattern.py
@@ -1,3 +1,20 @@
+from itertools import product
+
+
+def number_to_pattern_pythonic(index: int, k: int) -> str:
+    """Convert a number to its pattern representation using itertools 
+    cartesian product
+    
+    Arguments:
+        index {int} -- integer to convert to pattern
+        k {int} -- size of pattern
+
+    Returns:
+        str -- pattern
+    """
+    return "".join(list(product("ACGT", repeat=k))[index])
+
+
 def number_to_pattern(index: int, k: int) -> str:
     """Convert an integer to its pattern representation
     
