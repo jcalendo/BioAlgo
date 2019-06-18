eaebc9971c65efd62c9e696c99bc8769fda2566a
jcalendo <jcalendo@gmail.com>
2019-06-18 11:04:13 -0400

    updated tests

--- tests/test_stringAlgorithms.py
+++ tests/test_stringAlgorithms.py
@@ -1,34 +1,34 @@
 """
-Testing the functions in the stringAlgorithms module
+Testing the functions in the strings module
 """
 import pytest
-from BioAlgo.stringAlgorithms import patternCount, frequentWords, patternToNumber, numberToPattern, reverseComplement
+from BioAlgo.strings import convert, count, transform, words
 
 
 def test_pattern_count():
-    assert patternCount.pattern_count("ACAACTATGCATACTATCGGGAACTATCCT", "ACTAT") == 3
+    assert count.pattern_count("ACAACTATGCATACTATCGGGAACTATCCT", "ACTAT") == 3
 
 def test_frequent_words():
-    assert frequentWords.frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == {'ACTAT'}
+    assert words.frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == {'ACTAT'}
 
 def test_fast_frequent_words():
-    assert frequentWords.fast_frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == {'ACTAT'}
+    assert words.fast_frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == {'ACTAT'}
 
 def test_pattern_to_number():
-    assert patternToNumber.pattern_to_number("AGT") == 11
+    assert convert.pattern_to_number("AGT") == 11
 
 def test_pattern_to_number_pythonic():
-    assert patternToNumber.pattern_to_number_pythonic("GCGGTAA") == 9904
+    assert convert.pattern_to_number_pythonic("GCGGTAA") == 9904
 
 def test_number_to_pattern():
-    assert numberToPattern.number_to_pattern(11, 3) == "AGT"
+    assert convert.number_to_pattern(11, 3) == "AGT"
 
 def test_number_to_pattern_pythonic():
-    assert numberToPattern.number_to_pattern_pythonic(9904, 7) == "GCGGTAA"
+    assert convert.number_to_pattern_pythonic(9904, 7) == "GCGGTAA"
 
 def test_sorted_frequent_words():
-    assert frequentWords.sorted_frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == {'ACTAT'}
+    assert words.sorted_frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5) == {'ACTAT'}
 
 def test_reverse_complement():
-    assert reverseComplement.reverse_complement("AGTCGCATAGT") == "ACTATGCGACT"
+    assert transform.reverse_complement("AGTCGCATAGT") == "ACTATGCGACT"
 
