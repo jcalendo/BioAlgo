bcd6ebdd5c7b97c94c5352d1ae9622cbff193209
jcalendo <jcalendo@gmail.com>
2019-08-02 19:24:29 -0400

    added imediate_neighbors

--- bioalgo/strings/convert.py
+++ bioalgo/strings/convert.py
@@ -2,6 +2,7 @@
 Module for converting numbers to patterns and vice versa
 """
 from itertools import product
+from typing import List
 
 
 def number_to_pattern_pythonic(index: int, k: int) -> str:
@@ -131,3 +132,25 @@ def decompress_DNA_bit_string(bit_string: int) -> str:
             raise ValueError(f"Invalid DNA bit: {bits}")
 
     return dna_string[::-1]
+
+
+def imediate_neighbors(pattern: str) -> List[str]:
+    """Generate a list of neightbors whose hamming distance is 1 from the pattern
+    
+    Arguments:
+        pattern {str} -- pattern to generate neighborhood from
+
+    Returns:
+        List[str] -- 1-neighborhood list of pattern
+    """
+    neighbors = []
+    bases = ["A", "C", "G", "T"]
+    for i in range(len(pattern)):
+        symbol = pattern[i]
+        for base in bases:
+            if symbol != base:
+                pattern_list = list(pattern)
+                pattern_list[i] = base
+                neighbors.append("".join(pattern_list))
+
+    return neighbors
