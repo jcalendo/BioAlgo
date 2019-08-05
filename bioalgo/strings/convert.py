eaf61ea673ceff81cacbae0dda1c62cc7459dc2c
jcalendo <jcalendo@gmail.com>
2019-08-05 13:04:06 -0400

    moved imediate_neighbors from convert to count

--- bioalgo/strings/convert.py
+++ bioalgo/strings/convert.py
@@ -2,7 +2,6 @@
 Module for converting DNA strings 
 """
 from itertools import product
-from typing import List
 
 
 def number_to_pattern_pythonic(index: int, k: int) -> str:
@@ -132,27 +131,3 @@ def decompress_DNA_bit_string(bit_string: int) -> str:
             raise ValueError(f"Invalid DNA bit: {bits}")
 
     return dna_string[::-1]
-
-
-def imediate_neighbors(pattern: str) -> List[str]:
-    """Generate a list of neightbors whose hamming distance is 1 from the pattern
-    
-    Arguments:
-        pattern {str} -- pattern to generate neighborhood from
-
-    Returns:
-        List[str] -- 1-neighborhood list of pattern
-    """
-    neighbors = [pattern]
-    bases = ["A", "C", "G", "T"]
-    for i in range(len(pattern)):
-        symbol = pattern[i]
-        for base in bases:
-            if symbol != base:
-                pattern_list = list(pattern)
-                pattern_list[i] = base
-                neighbors.append("".join(pattern_list))
-
-    return neighbors
-
-print(imediate_neighbors("CAA"))
--- bioalgo/strings/count.py
+++ bioalgo/strings/count.py
@@ -1,7 +1,7 @@
 """
 Module for counting the occurences of patterns within a given text
 """
-from typing import Dict
+from typing import Dict, List
 from collections import Counter
 
 
@@ -89,3 +89,25 @@ def hamming_distance(str_1: str, str_2: str) -> int:
             count += 1
         
     return count
+
+
+def imediate_neighbors(pattern: str) -> List[str]:
+    """Generate a list of neightbors whose hamming distance is 1 from the pattern
+    
+    Arguments:
+        pattern {str} -- pattern to generate neighborhood from
+
+    Returns:
+        List[str] -- 1-neighborhood list of pattern
+    """
+    neighbors = [pattern]
+    bases = ["A", "C", "G", "T"]
+    for i in range(len(pattern)):
+        symbol = pattern[i]
+        for base in bases:
+            if symbol != base:
+                pattern_list = list(pattern)
+                pattern_list[i] = base
+                neighbors.append("".join(pattern_list))
+
+    return neighbors
